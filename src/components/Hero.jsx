0c633fea9b6df7fc6fd37385e9aaa93d8ce48287
MithunPara <paramatm@mcmaster.ca>
2025-05-22 16:25:43 -0400

    created an animated background gradient rotating colours

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,41 @@
 "use client"
-import React from 'react';
+import React, { useEffect } from 'react';
 import Image from 'next/image';
-import { motion } from "motion/react"
+import { animate, easeInOut, motion, useMotionTemplate, useMotionValue } from "motion/react"
 import headshotpic from "../assets/headshotpic.png"
 
+const GRADIENT_COLORS = ["#16a34a", "#0ea5e9", "#6366f1", "#f59e0b"];
 const Hero = () => {
   // bg-gradient-to-b from-zinc-900 via-green-600 to-sky-700
   // bg-[linear-gradient(to_bottom,#000,#2B1942_35%,#8F5C55_60%,#DBAF6E_80%)]
+  // [background:radial-gradient(125%_125%_at_50%_10%,#18181b_50%,#16a34a_100%)]
+  const color = useMotionValue(GRADIENT_COLORS[0]);
+  const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 10%, #18181b 50%, ${color} 100%)`;
+
+  useEffect(() => {
+    animate(color, GRADIENT_COLORS, {
+      ease: "easeInOut",
+      duration: 12,
+      repeat: Infinity,
+      repeatType: "mirror",
+    });
+  }, []);
+
   return (
-    <div className='py-24 relative overflow-clip min-h-screen bg-[linear-gradient(to_bottom,_#18181b_20%,_#16a34a_100%)]'>
-      <div>
-        <div className='text-7xl font-bold text-center'>
+    <motion.div style={{ backgroundImage }} className='py-24 relative overflow-clip min-h-screen flex items-center justify-center'>
+      <div className='text-center transform -translate-y-20'>
+        <div className='text-7xl font-bold'>
           <h1 className='text-slate-100'>Hello I'm</h1>
           <h1 className='text-emerald-200'>Mithun Paramathasan</h1>
         </div>
-
-        <span className='block text-xl text-center mx-auto mt-4 text-white/85'>Software Developer</span>
-
-        <Image
-          src={headshotpic}
-          alt="headshot picture"
-          className='absolute bottom-0 left-1/2 -translate-x-1/2 max-w-xs md:max-w-sm lg:max-w-md w-full object-contain'
-        /> 
+        <span className='block text-xl mt-4 text-white/85'>Software Developer</span>
       </div>
-    </div> 
+      <Image
+        src={headshotpic}
+        alt="headshot picture"
+        className='absolute bottom-0 left-1/2 -translate-x-1/2 w-auto h-[40vh] object-contain'
+      />  
+    </motion.div>
   )
 }
 
